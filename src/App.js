1d3004278df222ac56abc06a89083d56fbc524a4
Otakar Andrysek <otakar@MacBookPro.local>
2021-01-08 14:34:28 -0800

    First commit of my react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Component } from 'react'
+import Table from './Table'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+    render() {
+        const characters = [
+            {
+              name: 'Charlie',
+              job: 'Janitor',
+            },
+            {
+              name: 'Mac',
+              job: 'Bouncer',
+            },
+            {
+              name: 'Dee',
+              job: 'Aspring actress',
+            },
+            {
+              name: 'Dennis',
+              job: 'Bartender',
+            },
+        ]
 
-export default App;
+        return (
+            <div className="App">
+                    <h1>Hello, React!</h1>
+                <div className="container">
+                    <Table characterData={characters} />
+                </div>
+            </div>
+      )
+    }
+  }
+
+export default App
